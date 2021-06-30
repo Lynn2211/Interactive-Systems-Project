6e9b8ca87067e019e328e43a55930a270db67872
Lynn2211 <45513628+Lynn2211@users.noreply.github.com>
2021-06-30 18:13:37 +0200

    Update Project.ino
    conversion methods improved, changed input pin, fixed rotation, enabled unity communication

--- Project.ino
+++ Project.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
 //Set the pins that are used
-#define BUTTON_PIN 2
+#define BUTTON_PIN 12
 #define SERVO_PIN 0
 
 //True for output as hex
@@ -15,22 +15,27 @@ long startTime, endTime;
 //The list of dots and dashes of a letter
 String inputList = "";
 
+bool lightOn = false;
+
 Servo SERVO;
 
 void setup() {
   //Set the serial monitor to 9600 to enable output
   Serial.begin(9600);
-
+  
   //Attach the servo pin
-  SERVO.attach(SERVO_PIN);
+  SERVO.attach(SERVO_PIN, 544, 2500);
 
   //Set the button to pullup
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   //Wait for a button press and determine the length
-  while(digitalRead(BUTTON_PIN) == HIGH){yield();}
+  while(digitalRead(BUTTON_PIN) == HIGH){
+    takeInputFromUnity();
+  }
   startTime = millis();
   while(digitalRead(BUTTON_PIN) == LOW){yield();}
   endTime = millis();
@@ -71,11 +76,10 @@ void loop() {
   if(hexOutput){
     //Convert the letter to hexadecimal and output it to the serial monitor
     String outputAsHex = String(decRepresentation, HEX);
-    Serial.print(outputAsHex);
-    Serial.print(" ");
+    Serial.println(outputAsHex);
     }else{
       //Print the ascii letter
-      Serial.print(letter);
+      Serial.println(letter);
       }
   
 
@@ -94,76 +98,117 @@ void loop() {
     
   if(hexOutput){
     //Print space as hexadecimal
-    Serial.print("20");
-    Serial.print(" ");
+    Serial.println("20");
     }else{
       //Print a space
-      Serial.print(" ");
+      Serial.println(" ");
       }
   
 }
 
 
+void takeInputFromUnity(){
+  if(Serial.available() > 0){
+      if(Serial.read() == '1'){
+        if(lightOn){
+          digitalWrite(LED_BUILTIN, LOW);
+          lightOn = false;
+        }
+        else{
+         digitalWrite(LED_BUILTIN, HIGH);
+         lightOn = true;
+        }
+      }
+      if(Serial.read() == '2'){
+        //add another function
+      }
+    }
+}
+
 //Converts a morse input to a decimal of the corresponding letter
 //If the input doesn't match to a letter -1 is returned for error detection
 int morseToDecimal(String input){
-  if(input == ".-"){return 65;}
-  if(input == "-..."){return 66;}
-  if(input == "-.-."){return 67;}
-  if(input == "-.."){return 68;}
-  if(input == "."){return 69;}
-  if(input == "..-."){return 70;}
-  if(input == "--."){return 71;}
-  if(input == "...."){return 72;}
-  if(input == ".."){return 73;}
-  if(input == ".---"){return 74;}
-  if(input == "-.-"){return 75;}
-  if(input == ".-.."){return 76;}
-  if(input == "--"){return 77;}
-  if(input == "-."){return 78;}
-  if(input == "---"){return 79;}
-  if(input == ".--."){return 80;}
-  if(input == "--.-"){return 81;}
-  if(input == ".-."){return 82;}
-  if(input == "..."){return 83;}
-  if(input == "-"){return 84;}
-  if(input == "..-"){return 85;}
-  if(input == "...-"){return 86;}
-  if(input == ".--"){return 87;}
-  if(input == "-..-"){return 88;}
-  if(input == "-.--"){return 89;}
-  if(input == "--.."){return 90;}
-  return -1;
+  switch(input.length()){
+    case 1: 
+      if(input == "."){return 69;}
+      if(input == "-"){return 84;}
+      break;
+    case 2:
+      if(input == ".-"){return 65;}
+      if(input == ".."){return 73;}
+      if(input == "--"){return 77;}
+      if(input == "-."){return 78;}
+      break;
+    case 3:
+      if(input == "-.."){return 68;}
+      if(input == "--."){return 71;}
+      if(input == "-.-"){return 75;}
+      if(input == "---"){return 79;}
+      if(input == "..."){return 83;}
+      if(input == "..-"){return 85;}
+      if(input == ".--"){return 87;}
+      break;
+    case 4:
+      if(input == "-..."){return 66;}
+      if(input == "-.-."){return 67;}
+      if(input == "..-."){return 70;}
+      if(input == "...."){return 72;}
+      if(input == ".---"){return 74;}
+      if(input == ".-.."){return 76;}
+      if(input == ".--."){return 80;}
+      if(input == "--.-"){return 81;}
+      if(input == ".-."){return 82;}
+      if(input == "...-"){return 86;}
+      if(input == "-..-"){return 88;}
+      if(input == "-.--"){return 89;}
+      if(input == "--.."){return 90;}
+      break;
+    default:
+      return -1;
+      break;
+  }
 }
 
 //Converts a morse input to a decimal of the corresponding letter
 //If the input doesn't match to a letter e is returned for error detection
 char morseToChar(String input){
-  if(input == ".-"){return 'A';}
-  if(input == "-..."){return 'B';}
-  if(input == "-.-."){return 'C';}
-  if(input == "-.."){return 'D';}
-  if(input == "."){return 'E';}
-  if(input == "..-."){return 'F';}
-  if(input == "--."){return 'G';}
-  if(input == "...."){return 'H';}
-  if(input == ".."){return 'I';}
-  if(input == ".---"){return 'J';}
-  if(input == "-.-"){return 'K';}
-  if(input == ".-.."){return 'L';}
-  if(input == "--"){return 'M';}
-  if(input == "-."){return 'N';}
-  if(input == "---"){return 'O';}
-  if(input == ".--."){return 'P';}
-  if(input == "--.-"){return 'Q';}
-  if(input == ".-."){return 'R';}
-  if(input == "..."){return 'S';}
-  if(input == "-"){return 'T';}
-  if(input == "..-"){return 'U';}
-  if(input == "...-"){return 'V';}
-  if(input == ".--"){return 'W';}
-  if(input == "-..-"){return 'X';}
-  if(input == "-.--"){return 'Y';}
-  if(input == "--.."){return 'Z';}
-  return 'e';
+  switch(input.length()){
+    case 1:
+      if(input == "."){return 'E';}
+      if(input == "-"){return 'T';}
+      break;
+    case 2:
+      if(input == ".-"){return 'A';}
+      if(input == ".."){return 'I';}
+      if(input == "--"){return 'M';}
+      if(input == "-."){return 'N';}
+      break;
+    case 3:
+      if(input == "-.."){return 'D';}
+      if(input == "--."){return 'G';}
+      if(input == "-.-"){return 'K';}
+      if(input == "---"){return 'O';}
+      if(input == ".-."){return 'R';}
+      if(input == "..."){return 'S';}
+      if(input == "..-"){return 'U';}
+      if(input == ".--"){return 'W';}
+      break;
+    case 4:
+     if(input == "-..."){return 'B';}
+     if(input == "-.-."){return 'C';}
+     if(input == "..-."){return 'F';}
+     if(input == "...."){return 'H';}
+     if(input == ".---"){return 'J';}
+     if(input == ".-.."){return 'L';}
+     if(input == ".--."){return 'P';}
+     if(input == "--.-"){return 'Q';}
+     if(input == "...-"){return 'V';}
+     if(input == "-..-"){return 'X';}
+     if(input == "-.--"){return 'Y';}
+     if(input == "--.."){return 'Z';}
+     break;
+   default:
+     return 'e';
+     break;
+  }
 }
